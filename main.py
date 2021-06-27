f2d57261581a49d09d91edf7242dd5bf00377957
Anand <anandkrishnakumar123@gmail.com>
2021-06-27 15:39:19 +0300

    added main function

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ from deepQ import deepQtrain
 from tests import test
 import plots
 
-a = QAgent()
-episodes = 1000
-times_taken = Qtrain(Game, a)
-
-# testing with extreme initial state
-istate = np.zeros((X, Y)); istate[0, 0] = 1; istate[-1, -1] = 2
-test(Game, a)
+if __name__ == "__main__":
+    a = QAgent()
+    episodes = 1000
+    times_taken = Qtrain(Game, a)
+    
+    # testing with extreme initial state
+    istate = np.zeros((X, Y)); istate[0, 0] = 1; istate[-1, -1] = 2
+    test(Game, a)
